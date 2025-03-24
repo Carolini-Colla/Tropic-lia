068237672b8531ecd6d6e7c68c864d6fa0bf7c64
Carolini-Colla <carolini.colla@escola.pr.gov.br>
2025-03-24 19:37:19 -0300

    Add files via upload
    Botão de contraste

---
+++ Script.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function (){
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+
+    botaoDeAcessibilidade.addEventListener('click', function (){
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+    })
+
+    const aumentaFonteBotão = document.getElementById('aumentar-fonte'); 
+    const diminuiFonteBotão = document.getElementById('diminuir-fonte'); 
+
+    const alternaContraste = document.getElementById('alterna-contraste');
+
+    let tamanhoAtualFonte = 1; 
+
+    aumentaFonteBotão.addEventListener ('click', function(){
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+    })
+ 
+    diminuiFonteBotão.addEventListener ('click', function(){
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+    })
+
+    alternaContraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste')
+    })
+
+
+})
